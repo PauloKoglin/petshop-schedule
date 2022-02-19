9a781ab544fac8e39cbd921593de5562f0097f81
PauloKoglin <pk.koglin@gmail.com>
2022-02-19 23:07:52 +0100

    add SaveSchedule factory

--- src/main/factories/index.ts
+++ src/main/factories/index.ts
@@ -1,2 +1,3 @@
 export * from './load-schedule'
+export * from './save-schedule'
 export * from './localstorage'
---
+++ src/main/factories/save-schedule.ts
@@ -0,0 +1,9 @@
+import { makeLocalstorageCache } from ".";
+import { SaveSchedule } from "../../domain/use-cases";
+import { SaveScheduleInCache } from "../../services/use-cases";
+
+type Output = SaveSchedule
+
+export const makeSaveSchedule = (): Output => {
+    return new SaveScheduleInCache(makeLocalstorageCache())
+}
