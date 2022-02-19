bc583e4ba374a7b4aa71f3e68b0eda8d409df8ab
PauloKoglin <pk.koglin@gmail.com>
2022-02-19 11:22:10 +0100

    ensure formatTime returns time formated by hh:mm

--- src/presentation/utils/date-formatter.test.ts
+++ src/presentation/utils/date-formatter.test.ts
@@ -1,8 +1,17 @@
-import { formatDate } from "./date-formatter"
+import { formatDate, formatTime } from "./date-formatter"
 
 describe('date-formatter', () => {
     it('should return date in format of yyyy-MM-dd', () => {
         const result: String = formatDate(new Date('11-25-2022'))
+        
         expect(result).toBe('2022-11-25')
+    })
+
+    it('should return time in format of hh:mm', () => {        
+        const time = new Date('2022-01-01 10:30')
+
+        const result: String = formatTime(time.getTime())
+
+        expect(result).toBe('10:30')
     })
 })
--- src/presentation/utils/date-formatter.ts
+++ src/presentation/utils/date-formatter.ts
@@ -3,3 +3,7 @@ import fnsFormat from 'date-fns/format'
 export const formatDate = (date: Date): string => {
     return fnsFormat(date, 'yyyy-MM-dd')
 }
+
+export const formatTime = (time: number): string => {
+    return fnsFormat(time, 'HH:mm')    
+}
