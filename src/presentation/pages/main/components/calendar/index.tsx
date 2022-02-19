11e3e1027db7e9fbfe5edddef64590566a28c759
PauloKoglin <pk.koglin@gmail.com>
2022-02-19 15:15:33 +0100

    add schedule-modal to calendar component

--- src/presentation/pages/main/components/calendar/index.tsx
+++ src/presentation/pages/main/components/calendar/index.tsx
@@ -1,13 +1,14 @@
 import './styles.css'
+import ScheduleModal from '../../../../components/schedule-modal'
 
-import { Calendar as ReactCalendar, dateFnsLocalizer, SlotInfo } from 'react-big-calendar'
+import { useState } from 'react'
+import { Calendar as ReactCalendar, dateFnsLocalizer, SlotInfo, Event } from 'react-big-calendar'
 import 'react-big-calendar/lib/css/react-big-calendar.css'
 import format from 'date-fns/format'
 import parse from 'date-fns/parse'
 import startOfWeek from 'date-fns/startOfWeek'
 import getDay from 'date-fns/getDay'
 import enUS from 'date-fns/locale/en-US'
-import { useState } from 'react'
 
 const locales = {
   'en-US': enUS,
@@ -21,64 +22,53 @@ const localizer = dateFnsLocalizer({
   locales,
 })
 
-interface Event {
-    title: string,
-    start: Date,
-    end: Date,
-    allDay?: boolean
-    resource?: any
-}
-
-const initialState: Event[] = [
-  {
-    title: 'Meg Shower',
-    start: new Date(),
-    end: new Date(),
-  },
-  {
-    title: 'Clara Shower',
-    start: new Date(),
-    end: new Date(),
-  },
-  {
-    title: 'Cindy haircut',
-    start: new Date(),
-    end: new Date(),
-  }
-]
-
 const Calendar = () => {
-
-  const onSelectSlot = (slotInfo: SlotInfo) => {
-    const newEvent: Event = {
-      title: 'Event',
-      start: new Date(slotInfo.start),
-      end: new Date(slotInfo.end)
+    const [schedules] = useState<Event[]>([])  
+    const [schedulerModalVisible, setEventModalVisible] = useState<boolean>(false)
+    const [selectedSlot, setSelectedSlot] = useState<SlotInfo>()
+  
+    const onSelectSlot = (slotInfo: SlotInfo) => {
+        setSelectedSlot(slotInfo)
+        setEventModalVisible(true)
     }
-    setEvents([...events, newEvent])
-  }
 
-  const [events, setEvents] = useState<Event[]>(initialState)  
+    const getDefaultScheduleDate = (): Date => {
+        return selectedSlot?.start ? new Date(selectedSlot.start) : new Date()
+    }
+
+    const onCloseScheduler = () => {
+      setEventModalVisible(false)
+    }
+
+    const onScheduleSaved = () => {
+      setEventModalVisible(false)
+    }
     
-  return (
-    <div className='calendar'>
-      <ReactCalendar
-        localizer={localizer}
-        events={events}
-        selectable={true}
-        views={{
-          day: true,
-          week: true,
-          month: true
-        }}
-        startAccessor="start"
-        endAccessor="end"
-        onSelectSlot={(slotInfo: SlotInfo) => onSelectSlot(slotInfo)}
-      >
-          
-      </ReactCalendar>
-    </div>
-  );
+    return (
+        <div className='calendar'>
+            <ReactCalendar
+                localizer={localizer}
+                events={schedules}
+                selectable={true}
+                views={{
+                  day: true,
+                  week: true,
+                  month: true
+                }}
+                startAccessor="start"
+                endAccessor="end"
+                onSelectSlot={(slotInfo: SlotInfo) => onSelectSlot(slotInfo)}>
+            </ReactCalendar>
+            {
+              schedulerModalVisible &&
+              <ScheduleModal
+                  onScheduleClose={onCloseScheduler} 
+                  onScheduleSaved={onScheduleSaved}
+                  defaultDate={getDefaultScheduleDate()}   
+              />
+            }
+        </div>
+    );
 }
 
 export default Calendar
