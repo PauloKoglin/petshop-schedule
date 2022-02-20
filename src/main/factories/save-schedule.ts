fc2ffd1db6aac3803c5c4d83a355fe8d552fc636
PauloKoglin <pk.koglin@gmail.com>
2022-02-20 19:13:42 +0100

    fix LoadSchedule factory

--- src/main/factories/save-schedule.ts
+++ src/main/factories/save-schedule.ts
@@ -1,9 +1,12 @@
-import { makeLocalstorageCache } from ".";
+import { makeLoadSchedule, makeLocalstorageCache } from ".";
 import { SaveSchedule } from "../../domain/use-cases";
 import { SaveScheduleInCache } from "../../services/use-cases";
 
 type Output = SaveSchedule
 
 export const makeSaveSchedule = (): Output => {
-    return new SaveScheduleInCache(makeLocalstorageCache())
+    return new SaveScheduleInCache(
+        makeLocalstorageCache(),
+        makeLoadSchedule()
+    )
 }
