3762f58c1f0dfc744ef761cfe0b4b6b841092675
PauloKoglin <pk.koglin@gmail.com>
2022-02-20 19:14:37 +0100

    fix loadSchedule loadByMonth

--- src/services/use-cases/load-schedule/index.ts
+++ src/services/use-cases/load-schedule/index.ts
@@ -1,3 +1,4 @@
+import { Schedule } from '../../../domain/models'
 import { LoadSchedule } from '../../../domain/use-cases'
 import { CacheStorage } from '../../contracts'
 
@@ -9,9 +10,28 @@ export class LoadScheduleFromCache implements LoadSchedule {
         const cacheResult: any = this.cache.get('schedules')
         let schedules: LoadSchedule.Model = []
         if (cacheResult) {
-            schedules = [...cacheResult]
+            schedules = [...cacheResult].map(value => this.mapToSchedule(value))
             schedules = schedules.filter(schedule => schedule.startDate.getMonth() + 1 === input.month)            
         }
         return Promise.resolve(schedules)
-    }    
+    }
+
+    private mapToSchedule(value: any): Schedule {
+        return {
+            petName: value.petName,
+            ownerName: value.ownerName,
+            startDate: new Date(value.startDate),
+            endDate: new Date(value.endDate),
+            services: value.services
+        }
+    }
+
+    public loadAll(): Promise<LoadSchedule.Model> {
+        const cacheResult: any = this.cache.get('schedules')
+        let schedules: LoadSchedule.Model = []
+        if (cacheResult) {
+            schedules = [...cacheResult]
+        }
+        return Promise.resolve(schedules)
+    }
 }
